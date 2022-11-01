2694a8af8690b49f969056c6bdac5d6cc3b018df
ppranav06 <pranavkrishnap856@gmail.com>
2022-11-01 21:15:50 +0530

    added csv.QUOTE_ALL to dialect

--- CSV with quotes.py
+++ CSV with quotes.py
@@ -3,11 +3,11 @@
 import csv
 
 # Registering a dialect
-csv.register_dialect('myDialect', delimiter=',', skipinitialspace=True)
+csv.register_dialect('myDialect', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
 
 # Opening file in read mode
 f=open("Python/CSV Reader Function/CSV/quotes.csv", 'r')
-reader = csv.reader(f, dialect="myDialect", quoting=csv.QUOTE_ALL)
+reader = csv.reader(f, dialect="myDialect")
 
 # print each line row by row
 for row in reader:
